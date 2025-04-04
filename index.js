4ba912076b920665f810c64c7d3853ed0a49ba19
lionardus87 <yudhi_lee@hotmail.com>
2025-04-04 21:05:12 +1100

    add update button, search and filter category

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const expenses = [];
 
-const myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+const myModal = new bootstrap.Modal(document.getElementById("expenseModal"));
 let editId = null;
+let totalPaid = 0;
+let totalUnpaid = 0;
 
 function newExpense(id = null) {
 	if (id !== null) {
@@ -9,11 +11,13 @@ function newExpense(id = null) {
 		document.getElementById("formTitle").value = expense.title;
 		document.getElementById("formDesc").value = expense.desc;
 		document.getElementById("formAmount").value = expense.amount;
+		document.getElementById("formCategory").value = expense.category;
 		editId = id;
 	} else {
 		document.getElementById("formTitle").value = "";
 		document.getElementById("formDesc").value = "";
 		document.getElementById("formAmount").value = "";
+		document.getElementById("formCategory").value = "";
 		editId = null;
 	}
 
@@ -24,7 +28,8 @@ function saveExpenses() {
 	const title = document.getElementById("formTitle").value;
 	const desc = document.getElementById("formDesc").value;
 	const amount = document.getElementById("formAmount").value;
-	if (!title || !desc || !amount) {
+	const category = document.getElementById("formCategory").value;
+	if (!title || !desc || !amount || !category) {
 		alert("All fields are required");
 		return;
 	}
@@ -33,6 +38,7 @@ function saveExpenses() {
 		expenses[index].title = title;
 		expenses[index].desc = desc;
 		expenses[index].amount = amount;
+		expenses[index].category = category;
 	} else {
 		// Create new task
 		const id =
@@ -44,9 +50,10 @@ function saveExpenses() {
 			title,
 			desc,
 			amount,
+			category,
 			date: new Date().toLocaleString(),
-			// isCompleted: false,
-			// completedAt: null,
+			issued: false,
+			paidOn: null,
 		};
 		expenses.push(expense);
 	}
@@ -66,25 +73,138 @@ function deleteExpense(id) {
 	countExpenses();
 }
 
-function countExpenses() {
-	document.getElementById("expensesContainer").innerHTML = null;
-	document.getElementById("completedContainer").innerHTML = null;
-	expenses.forEach((expense) => {
-		const template = document
-			.getElementById("expensesList")
-			.content.cloneNode(true);
-		template.getElementById("id").innerText = expense.id;
-		template.getElementById("title").innerText = expense.title;
-		template.getElementById("desc").innerText = expense.desc;
-		template.getElementById("amount").innerText = `$${expense.amount}`;
-		template.getElementById("date").innerText = expense.date;
-
-		template.getElementById("delExpense").addEventListener("click", () => {
-			deleteExpense(expense.id);
-		});
-		document.getElementById("expensesContainer").appendChild(template);
-	});
-	// template
-	// 	.querySelector("button")
-	// 	.addEventListener("click", () => newExpense(expense.id));
+function paidExpense(id) {
+	const targetId = expenses.findIndex((expense) => expense.id == id);
+	const targetExpense = { ...expenses[targetId] };
+	targetExpense.issued = !targetExpense.issued;
+	targetExpense.paidOn = targetExpense.issued
+		? new Date().toLocaleString()
+		: null;
+	expenses[targetId] = targetExpense;
+	countExpenses();
 }
+
+function filterCategory() {
+	const categories = document.querySelectorAll(".dropdown-item");
+
+	categories.forEach((item) => {
+		item.addEventListener("click", function () {
+			const categoryValue = item.getAttribute("data-category");
+
+			document.getElementById("searchInput").value = "";
+
+			if (categoryValue === "allCategories") {
+				countExpenses();
+			} else {
+				const filtered = expenses.filter(
+					(expense) =>
+						expense.category.toLowerCase() === categoryValue.toLowerCase()
+				);
+				countExpenses(filtered);
+			}
+		});
+	});
+}
+
+function searchExpense() {
+	const searchQuery = document
+		.getElementById("searchInput")
+		.value.toLowerCase();
+	if (searchQuery === "") {
+		countExpenses();
+		return;
+	}
+	const filtered = expenses.filter((expense) => {
+		const titleMatch = expense.title.toLowerCase().includes(searchQuery);
+		const categoryMatch = expense.category.toLowerCase().includes(searchQuery);
+		const descMatch = expense.description.toLowerCase().includes(searchQuery);
+		return titleMatch || categoryMatch || descMatch;
+	});
+	countExpenses(filtered);
+}
+
+function sortExpenses() {
+	const sortOption = document.getElementById("sortOptions").value;
+
+	let unpaidExpenses = expenses.filter((exp) => !exp.issued);
+	let paidExpenses = expenses.filter((exp) => exp.issued);
+
+	const sortBy = (a, b) => {
+		if (sortOption === "price-low-high") {
+			return parseFloat(a.amount) - parseFloat(b.amount);
+		} else if (sortOption === "price-high-low") {
+			return parseFloat(b.amount) - parseFloat(a.amount);
+		} else if (sortOption === "title-asc") {
+			return a.title.localeCompare(b.title);
+		} else if (sortOption === "title-desc") {
+			return b.title.localeCompare(a.title);
+		}
+		return 0;
+	};
+
+	unpaidExpenses.sort(sortBy);
+	paidExpenses.sort(sortBy);
+
+	const sortedExpenses = [...unpaidExpenses, ...paidExpenses];
+	countExpenses(sortedExpenses);
+}
+
+function countExpenses(filteredExpenses = expenses) {
+	document.getElementById("expensesContainer").innerHTML = null;
+	document.getElementById("paidContainer").innerHTML = null;
+	totalPaid = 0;
+	totalUnpaid = 0;
+
+	filteredExpenses.forEach((expense) => {
+		if (expense) {
+			const template = document
+				.getElementById("expensesList")
+				.content.cloneNode(true);
+			template.getElementById("id").innerText = expense.id;
+			template.getElementById("title").innerHTML = expense.issued
+				? `<del>${expense.title}</del>`
+				: expense.title;
+			template.getElementById("desc").innerHTML = expense.issued
+				? `<del>${expense.title}</del>`
+				: expense.desc;
+			template.getElementById("amount").innerHTML = expense.issued
+				? `<del>$${expense.amount}</del>`
+				: `$${expense.amount}`;
+			template.getElementById("date").innerText = expense.issued
+				? expense.paidOn
+				: expense.date;
+
+			template.getElementById("category").innerText = expense.category;
+
+			//Check box
+			template.getElementById("paidExpense").checked = expense.issued;
+			template
+				.getElementById("paidExpense")
+				.addEventListener("click", () => paidExpense(expense.id));
+
+			//delete button
+			template.getElementById("delExpense").addEventListener("click", () => {
+				deleteExpense(expense.id);
+			});
+			//update button
+			template
+				.querySelector("button")
+				.addEventListener("click", () => newExpense(expense.id));
+
+			if (expense.issued) {
+				totalPaid += parseFloat(expense.amount);
+				document.getElementById("paidContainer").appendChild(template);
+			} else {
+				totalUnpaid += parseFloat(expense.amount);
+				document.getElementById("expensesContainer").appendChild(template);
+			}
+		}
+	});
+	document.getElementById("totalUnpaid").innerText = `$${totalUnpaid.toFixed(
+		2
+	)}`;
+	document.getElementById("totalPaid").innerText = `$${totalPaid.toFixed(2)}`;
+}
+
+countExpenses();
+filterCategory();
