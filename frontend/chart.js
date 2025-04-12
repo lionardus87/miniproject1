9c07c0e39173cd28325f99f388db99b8da6a7d9f
lionardus87 <yudhi_lee@hotmail.com>
2025-04-12 16:44:40 +1000

    add home page

--- frontend/chart.js
+++ frontend/chart.js
@@ -11,6 +11,7 @@ let options = {
 			name: "Amount",
 			type: "bar",
 			data: [],
+			itemStyle: {},
 		},
 	],
 };
@@ -18,9 +19,18 @@ let options = {
 function expensesChart() {
 	const titles = expenses.map((expense) => expense.title);
 	const amounts = expenses.map((expense) => parseFloat(expense.amount));
-
+	const colour = {
+		Utilities: "hsla(84, 83.50%, 45.30%, 0.98)",
+		Groceries: "rgba(83, 62, 244, 0.831)",
+		Entertainment: "rgb(236, 203, 40)",
+	};
 	options.xAxis.data = titles;
-	options.series[0].data = amounts;
+	options.series[0].data = expenses.map((expense) => ({
+		value: parseFloat(expense.amount),
+		itemStyle: {
+			color: colour[expense.category],
+		},
+	}));
 
 	myChart.setOption(options);
 }
@@ -28,6 +38,11 @@ let myChart = echarts.init(document.getElementById("main"));
 
 function expensesPieChart() {
 	const categoryMap = {};
+	const colour = {
+		Utilities: "hsla(84, 83.50%, 45.30%, 0.98)",
+		Groceries: "rgba(83, 62, 244, 0.831)",
+		Entertainment: "rgb(236, 203, 40)",
+	};
 
 	expenses.forEach((expense) => {
 		const type = expense.category;
@@ -40,20 +55,22 @@ function expensesPieChart() {
 	});
 
 	pieChart.setOption({
-		title: { text: "Total Ammount of each category" },
+		title: { text: "Category Summary" },
 		series: [
 			{
 				type: "pie",
 				data: Object.entries(categoryMap).map(([category, total]) => ({
 					name: category,
 					value: total,
+					itemStyle: {
+						color: colour[category],
+					},
 				})),
 			},
 		],
 	});
 }
 let pieChart = echarts.init(document.getElementById("pieChart"));
-
 window.onload = () => {
 	fetch("http://localhost:3000/expenses")
 		.then((response) => {
